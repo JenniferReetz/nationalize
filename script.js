effbe11892c60e08f928197ccd5bfac282a6d6c5
Stefanie Reetz <stefaniebeatrizreetz@gmail.com>
2026-05-29 19:38:20 -0300

    Adicionada outra api, para melhorar a visualização

--- script.js
+++ script.js
@@ -9,15 +9,28 @@ form.addEventListener('submit', function(event) {
 async function buscarNome(nome) {
     const resultado = document.getElementById('resultado');
     const url = `https://api.nationalize.io/?name=${nome}`;
+
     try {
+
         const resposta = await fetch(url);
         const dados = await resposta.json();
+
         if (dados.country.length === 0) {
+
             resultado.innerText = "Nenhum resultado encontrado.";
             return;
+
         }
-        const pais = dados.country[0].country_id;
+
+        const siglaPais = dados.country[0].country_id;
         const probabilidade = (dados.country[0].probability * 100).toFixed(2);
+
+        //outra api,para transformar em nome normal
+        const respostaPais = await fetch(`https://restcountries.com/v3.1/alpha/${siglaPais}`);
+
+        const dadosPais = await respostaPais.json();
+        const pais = dadosPais[0].name.common;
+
 
         resultado.innerText =
             `País mais provável: ${pais} (${probabilidade}%)`;
