8738205115f9faa045b71ea60724ad41751e7f51
Jennifer Reetz <jenniferheloisareetz@gmail.com>
2026-05-29 19:26:12 -0300

    Feito a lógica inicial do script

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('meuForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nome = document.getElementById('nome').value;
+    buscarNome(nome);
+});
+
+async function buscarNome(nome) {
+    const resultado = document.getElementById('resultado');
+    const url = `https://api.nationalize.io/?name=${nome}`;
+    try {
+        const resposta = await fetch(url);
+        const dados = await resposta.json();
+        if (dados.country.length === 0) {
+            resultado.innerText = "Nenhum resultado encontrado.";
+            return;
+        }
+        const pais = dados.country[0].country_id;
+        const probabilidade = (dados.country[0].probability * 100).toFixed(2);
+
+        resultado.innerText =
+            `País mais provável: ${pais} (${probabilidade}%)`;
+
+    } catch (erro) {
+        resultado.innerText = "Erro ao buscar dados.";
+        console.error(erro);
+    }
+}
